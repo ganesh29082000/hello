bb5622c23f05a903492232f7eeda3d2be1d7539d
ganesh29082000 <78561060+ganesh29082000@users.noreply.github.com>
2021-05-07 12:21:47 +0530

    Add files via upload

---
+++ EP_Project1/src/com/klu/Bean/RegistrationBean.java
@@ -0,0 +1,58 @@
+package com.klu.Bean;
+
+public class RegistrationBean {
+	
+	String name;
+	String email;
+	int age;
+	long pno;
+	String username;
+	String password;
+	String dance;
+
+	public String getDance() {
+		return dance;
+	}
+	public void setDance(String dance) {
+		this.dance = dance;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public long getPno() {
+		return pno;
+	}
+	public void setPno(long pno) {
+		this.pno = pno;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	
+
+}
